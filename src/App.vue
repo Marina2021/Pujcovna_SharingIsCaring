bb02c0ec64d91a32d5edde788ff8e4fcf253dc93
Lenka-da <64069604+Lenka-da@users.noreply.github.com>
2020-06-19 16:02:06 +0200

    import u App

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
- 
   <div id="app">
     <h1>Projekt půjčovna</h1>
-    
+
     <router-view />
 
     <hr />
     <router-link to="/">Na úvodní stránku</router-link>
-  
-</div>
-  
+  </div>
 </template>
 
 <script>
+import Footer from "@/components/Footer.vue";
+import Header from "@/components/Header.vue";
 
 export default {
-  name: 'App',
-}
-   
-  
+  name: "App",
+
+  components: {
+    footer: Footer,
+    header: Header
+  }
+};
 </script>
