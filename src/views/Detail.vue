fa52ac1f26647f27685d26ffbf80823064845f05
Lenka-da <64069604+Lenka-da@users.noreply.github.com>
2020-06-20 19:21:12 +0200

    detail

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-<<<<<<< HEAD
     <v-card>
       <div>
         <!-- <v-avatar class="ma-3" size="64" tile>
@@ -10,31 +9,13 @@
         <v-card-title class="headline" v-text="zobrazPolozku.nazev"></v-card-title>
       </div>
     </v-card>
-=======
-    <div>
-      <v-col class="col-6">
-        <v-card dark v-on:click="click">
-          <div class="vyska">
-            <v-avatar class="ma-3" size="64" tile>
-              <img v-bind:src="require(`./../assets/images/groups/${zobrazPolozku.image}`)" />
-            </v-avatar>
-x
-            <div>
-              <v-card-title class="headline" v-text="zobrazPolozku.name"></v-card-title>
-            </div>
-          </div>
-        </v-card>
-      </v-col>
-    </div>
->>>>>>> 6166d838a306a2c32b85d75ccc221dab0d277570
-  </div>
+</div>
 </template>
 ​
 <script>
 import Data from "@/assets/Data.js";
-​
+
 export default {
-<<<<<<< HEAD
   props: ["id"],
   data() {
     return {
@@ -46,12 +27,6 @@ export default {
       return this.products.filter(
         product => product.id === parseInt(this.id)
       )[0];
-=======
-   computed: {
-   zobrazPolozku: function() {
-      let polozka = this.products.filter(product => (product.id === parseInt(this.id)));
-      return zbozi;
->>>>>>> 6166d838a306a2c32b85d75ccc221dab0d277570
     }
   }
 };
