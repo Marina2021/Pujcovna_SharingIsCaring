37c4db22aaa6b81d20c292cc53561eb9504c747c
Marina <m.ivanova@email.cz>
2020-06-20 10:32:24 +0200

    linksZbozi

--- src/components/Kategorie.vue
+++ src/components/Kategorie.vue
@@ -1,15 +1,17 @@
 <template>
   <v-col class="col-4">
-    <v-card :color=blue dark>
-      <div class="d-flex flex-no-wrap justify-space-between">
-        <div>
-          <v-card-title class="headline" v-text="polozka.name"></v-card-title>
-        </div>
+    <v-card :color="blue" dark>
+      <router-link to="/zbozi">
+        <div class="d-flex flex-no-wrap justify-space-between">
+          <div>
+            <v-card-title class="headline" v-text="polozka.name"></v-card-title>
+          </div>
 
-        <v-avatar class="ma-3" size="64" tile>
-          <img v-bind:src="require(`./../assets/images/groups/${polozka.image}`)"/>
-        </v-avatar>
-      </div>
+          <v-avatar class="ma-3" size="64" tile>
+            <img v-bind:src="require(`./../assets/images/groups/${polozka.image}`)" />
+          </v-avatar>
+        </div>
+      </router-link>
     </v-card>
   </v-col>
 </template>
