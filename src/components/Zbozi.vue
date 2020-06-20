db519b633afa640ddf0940531d51cc3e389301cc
Lenka-da <64069604+Lenka-da@users.noreply.github.com>
2020-06-20 11:37:46 +0200

    foto zboží

--- src/components/Zbozi.vue
+++ src/components/Zbozi.vue
@@ -1,10 +1,43 @@
 <template>
 
 
- <v-card :color=blue dark>
+ <v-card>
       <div class="d-flex flex-no-wrap justify-space-between">
         <div>
-          <v-card-title class="headline" v-text="produkt.nazev"></v-card-title>
+  
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      src="/fotky_projekt_DA/elektro/mixer.jpg"
+    >
+      <v-card-title class="headline" v-text="produkt.nazev"></v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="pb-0">{{produkt.nazev}}</v-card-subtitle>
+
+    <v-card-text class="text--primary" >
+      <div>{{produkt.cena}} Kč/den</div>
+
+      <div>{{produkt.majitel}}</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn
+        color="orange"
+        text
+      >
+        Share
+      </v-btn>
+
+      <v-btn
+        color="orange"
+        text
+      >
+        Explore
+      </v-btn>
+    </v-card-actions>
+  
+          
         </div>
 
         <v-avatar class="ma-3" size="64" tile>
