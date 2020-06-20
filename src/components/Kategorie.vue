ab4dc9cc3e96b621291efe3069fb93cb843c985c
Marina <m.ivanova@email.cz>
2020-06-20 16:36:43 +0200

    routerlink update

--- src/components/Kategorie.vue
+++ src/components/Kategorie.vue
@@ -1,24 +1,28 @@
 <template>
   <v-col class="col-4">
-    <v-card dark>
-      <router-link v-bind:to="`/zbozi/${ polozka.id }`">
-        <div class="d-flex flex-no-wrap justify-space-between">
-          <div>
-            <v-card-title class="headline" v-text="polozka.name"></v-card-title>
-          </div>
-
-          <v-avatar class="ma-3" size="64" tile>
-            <img v-bind:src="require(`./../assets/images/groups/${polozka.image}`)" />
-          </v-avatar>
+    <v-card dark v-on:click="click">
+      <div class="d-flex flex-no-wrap justify-space-between">
+        <div>
+          <v-card-title class="headline" v-text="polozka.name"></v-card-title>
         </div>
-      </router-link>
+
+        <v-avatar class="ma-3" size="64" tile>
+          <img v-bind:src="require(`./../assets/images/groups/${polozka.image}`)" />
+        </v-avatar>
+      </div>
     </v-card>
   </v-col>
 </template>
 
 <script>
+
 export default {
-  props: ["polozka"]
+  props: ["polozka"],
+  methods: {
+    click: function() {
+     this.$router.push({ name: `Zbozi`, params: { id: this.polozka.id } })
+    }
+  }
 };
 </script>
 
