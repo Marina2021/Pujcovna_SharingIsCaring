a6cde6515f261019a960c2f9c1ba4c3ea890daf3
Marina <m.ivanova@email.cz>
2020-06-21 12:55:11 +0200

    popis detail orange

--- src/components/Kategorie.vue
+++ src/components/Kategorie.vue
@@ -1,49 +1,39 @@
 <template>
-
   <v-col class="col-6">
     <v-card v-bind:color="polozka.barva" dark v-on:click="click">
       <div class="vyska">
-        
-
-        <v-avatar class="ma-3" size="64" tile>
-          <img v-bind:src="require(`./../assets/images/groups/${polozka.image}`)" />
-        </v-avatar>
-
-        
-
-        <div>
+        <div class="d-flex justify-center">
+          <v-avatar class="ma-3 justify-center" tile>
+            <img v-bind:src="require(`./../assets/images/groups/${polozka.image}`)" />
+          </v-avatar>
+        </div>
+        <div class="d-flex justify-center">
           <v-card-title class="text" v-text="polozka.name"></v-card-title>
         </div>
       </div>
     </v-card>
   </v-col>
-
 </template>
 
 <script>
 export default {
   props: ["polozka"],
   methods: {
-
     click: function() {
-     this.$router.push({ name: `Zbozi`, params: { id: this.polozka.id } })
+      this.$router.push({ name: `Zbozi`, params: { id: this.polozka.id } });
     }
   }
 };
 </script>
 
 <style>
-
-.vyska{
-  flex:box;
-  flex-direction:column;
-
+.vyska {
+  flex: box;
+  flex-direction: column;
 }
 
-.headline{
-  font-size:5px;
+.headline {
+  font-size: 5px;
   padding: 5px;
 }
-
-
 </style>
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -5,39 +5,21 @@
         <v-img v-bind:src="`/fotky_projekt_DA/${zobrazPolozku.images}`" height="200px"></v-img>
 
         <v-card-title class="headline" v-text="zobrazPolozku.nazev">
-          <!--<div>
-            <div class="headline">{{ zobrazPolozku.nazev }}</div>
-            <span class="grey--text">{{ zobrazPolozku.cena }}</span>
-            <span class="grey--text">{{ zobrazPolozku.mesto }}</span>
-          </div>-->
+          
         </v-card-title>
 
         <v-card-subtitle>{{ zobrazPolozku.cena }} Kč/den</v-card-subtitle>
         <v-card-subtitle>{{ zobrazPolozku.mesto }}</v-card-subtitle>
         <v-card-text>{{ zobrazPolozku.popis }}</v-card-text>
         <v-card-actions>
-          <!-- <v-btn flat>{{ zobrazPolozku.mesto }}</v-btn> 
-          <v-btn flat color="orange">Pujčit</v-btn>-->
-          <div class="text-center">
-            <v-bottom-sheet v-model="sheet" inset>
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn color="orange" dark v-bind="attrs" v-on="on">Pujčit</v-btn>
-              </template>
-              <v-sheet class="text-center" height="200px">
-                <v-btn class="mt-6" text color="error" @click="sheet = !sheet">close</v-btn>
-                <div class="my-3">Tady budou kontaktni detaily majitele</div>
-              </v-sheet>
-            </v-bottom-sheet>
-          </div>
-          <v-spacer></v-spacer>
-          <!-- <v-btn icon @click="show = !show">
-            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-          </v-btn> -->
+          <div class="d-flex justify-center">
+          
+            <v-btn flat color="orange">Pujčit</v-btn>
+           </div>
+          
         </v-card-actions>
 
-        <!-- <v-slide-y-transition>
-          <v-card-text v-show="show">{{ zobrazPolozku.popis }}</v-card-text>
-        </v-slide-y-transition>-->
+        
       </v-card>
     </v-flex>
   </v-layout>
@@ -51,7 +33,7 @@ export default {
   props: ["id"],
   data() {
     return {
-      products: Data.products,
+      products: Data.products
     };
   },
   computed: {
