980be1b6c60ea46622fa640f35b2c4b569655e96
Marina <m.ivanova@email.cz>
2020-06-21 13:26:05 +0200

    center the button pujcit

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -3,16 +3,12 @@
     <v-flex xs12 sm6 offset-sm3>
       <v-card>
         <v-img v-bind:src="`/fotky_projekt_DA/${zobrazPolozku.images}`" height="200px"></v-img>
-
         <v-card-title class="headline" v-text="zobrazPolozku.nazev"></v-card-title>
-
         <v-card-subtitle>{{ zobrazPolozku.cena }} Kč/den</v-card-subtitle>
         <v-card-subtitle>{{ zobrazPolozku.mesto }}</v-card-subtitle>
         <v-card-text>{{ zobrazPolozku.popis }}</v-card-text>
-        <v-card-actions>
-          <div class="d-flex justify-center">
-            <v-btn v-on:click="proclick" flat color="orange">Pujčit</v-btn>
-          </div>
+        <v-card-actions class="d-flex justify-center">
+            <v-btn flat color="orange">Pujčit</v-btn>
         </v-card-actions>
       </v-card>
     </v-flex>
